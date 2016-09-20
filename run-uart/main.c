075039877c12df9a6de8f1d1055dc705d47d1280
lupyuen-unabiz <ly.lee@unabiz.com>
2016-09-21 02:01:56 +0800

    Fixing linker script

--- run-uart/main.c
+++ run-uart/main.c
@@ -1,4 +1,4 @@
-/*
+#include <sys/cdefs.h>/*
 This example sets D8/D9 as pin mode to UART and then opens UART1 device.
 It sets UART settings such as baud rate, stop bits, and register read callback.
 When there are some data available to the device through UART1.
@@ -19,10 +19,10 @@ It reads data with vm_dcl_read and then writes same data through UART1 with vm_d
 
 //  LED to GPIO mapping, according to https://github.com/loboris/RePhone_on_Linux/blob/master/Documents/Lua%20on%20RePhone%20Manual.pdf
 const int MAX_LED = 3;
-const int GPIO_LED[3] = { 17 /* red */, 15 /* green */, 12 /* blue */ };
+const unsigned int GPIO_LED[3] = { 17 /* red */, 15 /* green */, 12 /* blue */ };
 VM_DCL_HANDLE gpio_handle[3] = {0, 0, 0};  //  Handle for each GPIO LED.
 
-VM_TIMER_ID_PRECISE sys_timer_id = 0;
+VM_TIMER_ID_PRECISE  __unused sys_timer_id = 0;
 VM_DCL_HANDLE g_uart_handle = 0;  /* handle of UART */
 VM_DCL_OWNER_ID g_owner_id = 0;  /* Module owner of APP */
 vm_dcl_sio_control_dcb_t g_config;  /* UART configuration setting */
@@ -45,15 +45,15 @@ void log_to_file(const char *log)
     else {
         snprintf(buffer2, sizeof(buffer2), "%s", log);
     }
-    VM_FS_HANDLE filehandle = -1;
     VMUINT writelen = 0;
     vm_log_info(log);
     if (filename[0] == 0) {  //  Compose the filename c:\log.txt.
-        snprintf(filename, sizeof(filename), "%c:\\%s", vm_fs_get_internal_drive_letter(), "log.txt");
+        snprintf((char *) filename, VM_FS_MAX_PATH_LENGTH, "%c:\\%s", vm_fs_get_internal_drive_letter(), "log.txt");
         vm_chset_ascii_to_ucs2(wfilename, sizeof(wfilename), filename);
     }
     //  Create file.
-    if ((filehandle = vm_fs_open(wfilename, VM_FS_MODE_APPEND, TRUE)) < 0) {
+    VM_FS_HANDLE filehandle = vm_fs_open(wfilename, VM_FS_MODE_APPEND, TRUE);
+    if (filehandle < 0) {
         vm_log_info("log_to_file: Failed to create file: %s",filename);
         return;
     }
@@ -77,44 +77,42 @@ void gpio_init(void)
     }
 }
 
-void uart_irq_handler(void* parameter, VM_DCL_EVENT event, VM_DCL_HANDLE device_handle){
+void uart_irq_handler(void __unused * parameter, VM_DCL_EVENT event, VM_DCL_HANDLE device_handle){
     //  Handle received UART data.
     if(event == VM_DCL_SIO_UART_READY_TO_READ){
         //  Data has arrived.
         VMCHAR data[256];
-        VMINT i;
         VM_DCL_STATUS status;
         VM_DCL_BUFFER_LENGTH returned_len;
-        VMINT count = 0;
         //  Read data into buffer.
-        status = vm_dcl_read(device_handle,(VM_DCL_BUFFER*)data, sizeof(data), &returned_len, vm_dcl_get_owner_id());
+        status = vm_dcl_read(device_handle,(VM_DCL_BUFFER*)data, sizeof(data), &returned_len, (unsigned int) vm_dcl_get_owner_id());
         if(status < VM_DCL_STATUS_OK){
             log_to_file("uart_irq_handler: read failed");
             return;
         }
         led_color = 0;  //  Blink Red when data received.
-        snprintf(buffer, sizeof(buffer), "uart_irq_handler: read length = %d", returned_len); log_to_file(buffer);
+        snprintf(buffer, sizeof(buffer), "uart_irq_handler: read length = %d", (int) returned_len); log_to_file(buffer);
         if (returned_len >= 0) buffer[returned_len] = 0;
-        log_to_file(data);
+        log_to_file((char *) data);
     }
 }
 
-void send_uart_data(unsigned char data[], int len) {
+void send_uart_data(unsigned char data[], unsigned int len) {
     //  Send data to UART port.
     snprintf(buffer, sizeof(buffer), "send_uart_data: length = %d", len); log_to_file(buffer);
-    log_to_file(data);
+    log_to_file((char *) data);
     if(g_uart_handle != -1){
         //  Write data
         VM_DCL_BUFFER_LENGTH write_len = 0;
-        VM_DCL_STATUS status = vm_dcl_write(g_uart_handle, (VM_DCL_BUFFER*)data, len, &write_len, vm_dcl_get_owner_id());
+        VM_DCL_STATUS status = vm_dcl_write(g_uart_handle, data, len, &write_len, (unsigned int) vm_dcl_get_owner_id());
         //  Continue to write data if write fails.
         VMINT count = 0;
         while((status < VM_DCL_STATUS_OK || write_len != len) && (count < 3))
         {
             count++;
-            status = vm_dcl_write(g_uart_handle,(VM_DCL_BUFFER*)data, len, &write_len, vm_dcl_get_owner_id());
+            status = vm_dcl_write(g_uart_handle, data, len, &write_len, (unsigned int) vm_dcl_get_owner_id());
         }
-        snprintf(buffer, sizeof(buffer), "send_uart_data: write length = %d", write_len); log_to_file(buffer);
+        snprintf(buffer, sizeof(buffer), "send_uart_data: write length = %d", (int) write_len); log_to_file(buffer);
     }
     else {
         log_to_file("send_uart_data: UART not opened yet");
@@ -124,7 +122,7 @@ void send_uart_data(unsigned char data[], int len) {
 //  Sample command to be sent.
 const char cmd[] = "AT\r";
 
-void sys_timer_callback(VM_TIMER_ID_PRECISE sys_timer_id, void* user_data)
+void sys_timer_callback(VM_TIMER_ID_PRECISE  __unused sys_timer_id, void __unused * user_data)
 {
     //  This call back happens every 5 seconds.  We flash blue if no data received, else we flash red.
     static int out = 0;
@@ -142,7 +140,7 @@ void sys_timer_callback(VM_TIMER_ID_PRECISE sys_timer_id, void* user_data)
     send_uart_data((unsigned char *) cmd, strlen(cmd));
 }
 
-void handle_sysevt(VMINT message, VMINT param) {
+void handle_sysevt(VMINT message, VMINT  __unused param) {
     //  The callback to be invoked by the system engine.
     switch (message){
         case VM_EVENT_CREATE:
@@ -154,12 +152,12 @@ void handle_sysevt(VMINT message, VMINT param) {
             vm_dcl_config_pin_mode(VM_PIN_P9, VM_DCL_PIN_MODE_UART);
             g_owner_id = vm_dcl_get_owner_id();
             //  Open UART1.
-            g_uart_handle = vm_dcl_open(VM_DCL_SIO_UART_PORT1, vm_dcl_get_owner_id());
+            g_uart_handle = vm_dcl_open(VM_DCL_SIO_UART_PORT1, (unsigned int) vm_dcl_get_owner_id());
             if(VM_DCL_HANDLE_INVALID == g_uart_handle){
                 log_to_file("handle_sysevt: UART open failed");
             }
             else{
-                g_config.owner_id = g_owner_id;
+                g_config.owner_id = (unsigned int) g_owner_id;
                 g_config.config.dsr_check = 0;
                 g_config.config.data_bits_per_char_length = VM_DCL_SIO_UART_BITS_PER_CHAR_LENGTH_8;
                 g_config.config.flow_control = VM_DCL_SIO_UART_FLOW_CONTROL_NONE;
@@ -171,12 +169,14 @@ void handle_sysevt(VMINT message, VMINT param) {
                 //  Configure UART1 for 9600 baud.
                 vm_dcl_control(g_uart_handle, VM_DCL_SIO_COMMAND_SET_DCB_CONFIG,(void *)&g_config);
                 //  Resgiter callback when data received.
-                vm_dcl_register_callback(g_uart_handle, VM_DCL_SIO_UART_READY_TO_READ, (vm_dcl_callback)uart_irq_handler, (void*)NULL);
+                vm_dcl_register_callback(g_uart_handle, VM_DCL_SIO_UART_READY_TO_READ, (vm_dcl_callback)uart_irq_handler, NULL);
             }
             log_to_file("handle_sysevt: Initialised");
             break;
         case VM_EVENT_QUIT:
             vm_dcl_close(g_uart_handle);
+            break;
+        default:
             break;
     }
 }
@@ -184,12 +184,11 @@ void handle_sysevt(VMINT message, VMINT param) {
 void test_log()
 {
     //  Write the log to the log file c:\log.txt and debug console.
-    VM_FS_HANDLE filehandle = -1;
     VMUINT writelen = 0;
-    strcpy(filename, "c:\\log1.txt");
+    strcpy((char *) filename, "c:\\log1.txt");
     vm_chset_ascii_to_ucs2(wfilename, sizeof(wfilename), filename);
     //  Create file.
-    filehandle = vm_fs_open(wfilename, VM_FS_MODE_APPEND, FALSE);
+    VM_FS_HANDLE filehandle = vm_fs_open(wfilename, VM_FS_MODE_APPEND, FALSE);
     if (filehandle < 0) return;
     //  Write file.
     VM_RESULT ret = vm_fs_write(filehandle, "test\n", 5, &writelen);
@@ -198,7 +197,7 @@ void test_log()
     vm_fs_close(filehandle);
 }
 
-void vm_main(void){
+void __unused vm_main(void){
     test_log();
     //  This is the entry point for the program. We register the handlers for system events.
     vm_pmng_register_system_event_callback(handle_sysevt);
